a31aba3841ee3d9812948e42350a76d535fe5a6a
Quoc Tran <quoctran1998@gmail.com>
2023-05-20 00:31:40 -0700

    Moved help to a separate page

--- server/routes/main.py
+++ server/routes/main.py
@@ -17,9 +17,9 @@ def index():
 def profile():
     return(render_template("profile.html", name=current_user.name))
 
-@main.route("/about")
-def about():
-    return(render_template("about.html"))
+@main.route("/help")
+def help():
+    return(render_template("help.html"))
 
 @main.route("/invite")
 def invite():
